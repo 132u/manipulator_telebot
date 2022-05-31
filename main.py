40cdcb657ea99cef859ffadc16ddac9b260154e3
Irina <132u@mail.ru>
2022-05-31 06:16:42 +0300

    s

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import telebot
 import os
 import logging
@@ -16,9 +15,11 @@ if __name__ == "__main__":
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
 order = Order()
 user = User()
 order_message = ""
+
 @bot.message_handler(commands=['start', 'привет'])
 def start(message):
     # Вот создание кнопки, можете поменять некоторые значения, и посмотреть что будет
