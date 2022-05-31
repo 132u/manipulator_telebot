ff7fa10e128b10515feb2bac5decf19b3c178eed
Irina <132u@mail.ru>
2022-05-31 07:46:10 +0300

    q

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def start(message):
     bot.send_message(message.chat.id, f'Здравствуйте, {message.from_user.username}!\nЯ бот для заказа эвакуатора или манипулятора.\nЦена подачи 2500руб, если вас устраивает цена, то давайте оформим заявку, выберите услугу', reply_markup=markup_inline)
 
 #перенаправление входящих сообщений с сервера фласк к боту
-@server.route(f"/{BOT_TOKEN}", methods=["POST"])
+@server.route(f"{APP_URL}{BOT_TOKEN}", methods=["POST"])
 def redirect_message():
     #получаем данные от сервера в json, декодируем и отправляем боту
     json_string = request.get_data().decode("utf-8")
