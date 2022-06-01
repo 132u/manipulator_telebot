f624565703afdf2f597345655bebcfead22f32a1
132u <132u@mail.ru>
2022-06-01 12:08:19 +0300

    Update main.py
    a

--- main.py
+++ main.py
@@ -12,13 +12,18 @@ logger = telebot.logger
 logger.setLevel(logging.DEBUG)
 
 if __name__ == "__main__":
-    bot.remove_webhook()
-    bot.set_webhook(url=APP_URL + BOT_TOKEN)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 order = Order()
 user = User()
 order_message = ""
+
+@server.route("/")
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url=APP_URL)
+    return "!", 200
+    
 
 @bot.message_handler(commands=['start', 'привет'])
 def start(message):
